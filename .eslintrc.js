d23a29ec42c587ec202ccc778151b632f315aa0f
GuillaumeFerron <guillaume.ferron2@gmail.com>
2018-11-01 09:48:21 -0400

    eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,15 @@
 module.exports = {
   extends: [
     // add more generic rulesets here, such as:
-    // 'eslint:recommended',
-    'plugin:vue/essential'
+    'plugin:vue/essential',
+    'airbnb'
   ],
   rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'comma-dangle': 'off',
+    'space-before-function-paren': 'off',
+    'import/no-unresolved': 'off',
+    'object-curly-spacing': 'off'
   }
 }
