a5702a67682aef6dcf7b8c82c03040ef4f0fe16a
GuillaumeFerron <guillaume.ferron2@gmail.com>
2018-12-02 13:10:28 -0500

    Test for seo title

--- test/index.test.js
+++ test/index.test.js
@@ -19,20 +19,23 @@ test.before('Init Nuxt.js', async t => {
   nuxt.listen(4000, 'localhost')
 })
 
-// Example of testing only generated html
-test('Route / exits and render HTML', async t => {
-  const context = {}
-  const { html } = await nuxt.renderRoute('/', context)
-  t.true(html.includes('<h1 class="title">nuxt-boilerplate</h1>'))
-})
-
 // Example of testing via DOM checking
 test('Route / exits and render HTML with CSS applied', async t => {
   const window = await nuxt.renderAndGetWindow('http://localhost:4000/')
-  const element = window.document.querySelector('.subtitle')
-  t.not(element, null)
-  t.is(element.textContent, 'A boilerplate for Nuxt projects. It includes Bootstrap | SCSS.')
-  t.is(element.className, 'subtitle')
+  const seoTitle = window.document.querySelector('.seo-title')
+  t.not(seoTitle, null)
+
+  // Content check
+  t.is(seoTitle.textContent, window.document.title)
+
+  // CSS check
+  t.is(window.getComputedStyle(seoTitle).opacity, 0)
+  t.is(window.getComputedStyle(seoTitle).zIndex, '-99999999')
+  t.is(window.getComputedStyle(seoTitle).top, '-99999999px')
+  t.is(window.getComputedStyle(seoTitle).left, '-99999999px')
+  t.is(window.getComputedStyle(seoTitle).position, 'absolute')
+  t.is(window.getComputedStyle(seoTitle).maxHeight, '0')
+  t.is(window.getComputedStyle(seoTitle).maxWidth, '0')
 })
 
 // Close the Nuxt server
