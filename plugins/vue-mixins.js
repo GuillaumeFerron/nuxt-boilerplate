88759436a8e7352fa6e066108fb2df647c56df03
GuillaumeFerron <guillaume.ferron2@gmail.com>
2018-12-13 15:49:13 -0500

    Documentation

--- plugins/vue-mixins.js
+++ plugins/vue-mixins.js
@@ -1,6 +1,10 @@
 /* eslint import/no-extraneous-dependencies: 0 */
 /* eslint no-unused-vars: 0 */
 
+/**
+ * This file registers some mixins globally
+ */
+
 import Vue from 'vue'
 
 import miscellaneous from '@/mixins/miscellaneous'
--- store/index.js
+++ store/index.js
@@ -7,21 +7,64 @@ import Vuex from 'vuex'
 const createStore = () => new Vuex.Store({
   state: () => ({
     loaded: false,
-    isMobile: false
+    isMobile: false,
+    cachedRoutes: []
   }),
   getters: {
     loaded: state => state.loaded,
-    isMobile: state => state.isMobile
+    isMobile: state => state.isMobile,
+    cachedRoutes: state => state.cachedRoutes
   },
   mutations: {
+    /**
+     * The website has been loaded once
+     *
+     * @param state
+     * @constructor
+     */
     SITE_LOADED(state) {
       state.loaded = true
     },
+
+    /**
+     * Update the isMobile store data
+     *
+     * @param state
+     * @param value
+     * @constructor
+     */
     CHANGE_MOBILE(state, value) {
       state.isMobile = value
+    },
+
+    /**
+     * Update the cached routes array
+     *
+     * @param state
+     * @param route
+     * @constructor
+     */
+    UPDATE_CACHED_ROUTES(state, route) {
+      state.cachedRoutes.push(route)
+    },
+
+    /**
+     * Resets the cached routes array
+     *
+     * @param state
+     * @constructor
+     */
+    RESET_CACHED_ROUTES(state) {
+      state.cachedRoutes = [].slice(0)
     }
   },
   actions: {
+    /**
+     * Actions to be done on first website load
+     *
+     * @param state
+     * @param commit
+     */
     init({ state, commit }) {
       window.addEventListener('resize', () => {
         commit('CHANGE_MOBILE', $(window).width() <= 768)
