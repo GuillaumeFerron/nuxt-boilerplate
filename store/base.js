0e2694be3e178d15a225f57761a6a299c1f8dc44
GuillaumeFerron <guillaume.ferron2@gmail.com>
2019-11-10 19:25:28 -0500

    Set browser in the state

--- store/base.js
+++ store/base.js
@@ -2,7 +2,8 @@
 export const state = () => ({
   loaded: false,
   cachedRoutes: [],
-  screenSize: false
+  screenSize: false,
+  browser: ''
 })
 
 export const getters = {
@@ -52,6 +53,16 @@ export const mutations = {
    */
   SET_SCREEN_SIZE(state, value) {
     state.screenSize = value
+  },
+  /**
+   * Set browser
+   *
+   * @param state
+   * @param value
+   * @constructor
+   */
+  SET_BROWSER(state, value) {
+    state.browser = value
   }
 }
 export const actions = {
@@ -65,8 +76,41 @@ export const actions = {
     commit('SET_SCREEN_SIZE', $(window).width() < 768 ? 'xs' : $(window).width() < 992 ? 'sm' : $(window).width() < 1200 ? 'md' : 'lg')
 
     window.addEventListener('resize', () => {
-      console.log('rest')
       commit('SET_SCREEN_SIZE', $(window).width() < 768 ? 'xs' : $(window).width() < 992 ? 'sm' : $(window).width() < 1200 ? 'md' : 'lg')
     })
+
+    commit('SET_BROWSER', checkBrowser())
+  }
+}
+
+/**
+ * Check for browser support
+ */
+export function checkBrowser() {
+  // Opera 8.0+
+  if ((!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0) {
+    return 'opera'
+  }
+  // Firefox 1.0+
+  if (typeof InstallTrigger !== 'undefined') {
+    return 'firefox'
+  }
+  // Safari 3.0+ "[object HTMLElementConstructor]"
+  if (/constructor/i.test(window.HTMLElement) || (function(p) {
+    return p.toString() === '[object SafariRemoteNotification]'
+  })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification))) {
+    return 'safari'
+  }
+  // Internet Explorer 6-11
+  if (/* @cc_on!@*/false || !!document.documentMode) {
+    return 'ie'
+  }
+  // Edge 20+
+  if (window.StyleMedia) {
+    return 'edge'
+  }
+  // Chrome 1 - 71
+  if (!!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime)) {
+    return 'chrome'
   }
 }
